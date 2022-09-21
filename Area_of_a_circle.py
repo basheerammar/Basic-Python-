bf60047b1b1b37a7093629d95560b3c40efcab94
AMR <96076497+basheerammar@users.noreply.github.com>
2022-09-21 11:52:58 +0500

    Update Area_of_a_circle.py

--- Area_of_a_circle.py
+++ Area_of_a_circle.py
@@ -1,6 +1,6 @@
 import math
 
-radius = float(input("What is the radius of your circle? ")) # I couldnt get PI from the built in library :(
+radius = float(input("What is the radius of your circle? ")) 
 pi = math.pi
 area = pi*radius**2
 print("Area =", round(area,2))
